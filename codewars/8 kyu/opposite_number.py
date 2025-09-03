02ce5e5389ead914778f22e31c240c242fab7915
i-chernousov <iche7n@gmail.com>
2025-09-03 19:34:31 +0300

    Добавлено решение Codewars 8KYU

---
+++ codewars/8 kyu/opposite_number.py
@@ -0,0 +1,12 @@
+"""
+Very simple, given a number (integer / decimal / both depending on the language),
+find its opposite (additive inverse).
+"""
+
+def opposite(number: float) -> float:
+    """ Функция dозвращает аддитивную противоположность числа (меняет знак)"""
+    return -number
+
+
+if __name__ == "__main__":
+    print(opposite(1))
