db989ff798c136cbb02a5e2b450a0bd1b1d746bc
i-chernousov <iche7n@gmail.com>
2025-09-05 21:36:14 +0300

    Добавлено решение для Codewars № 2
    Calculate_bmi

---
+++ codewars/8 kyu/calculate_bmi.py
@@ -0,0 +1,31 @@
+"""
+Write function bmi that calculates body mass index (bmi = weight / height2).
+
+if bmi <= 18.5 return "Underweight"
+
+if bmi <= 25.0 return "Normal"
+
+if bmi <= 30.0 return "Overweight"
+
+if bmi > 30 return "Obese"
+
+"""
+
+def bmi(weight: float, height: float) -> str:
+    """ Рассчитывает индекс массы тела (BMI) """
+    calculate_bmi = weight / (height ** 2)
+
+    if calculate_bmi <= 18.5:
+        return "Underweight"
+
+    elif calculate_bmi <= 25.0:
+        return "Normal"
+
+    elif calculate_bmi <= 30.0:
+        return "Overweight"
+
+    else:
+        return "Obese"
+
+if __name__ == "__main__":
+    print(bmi(83, 1.85))
