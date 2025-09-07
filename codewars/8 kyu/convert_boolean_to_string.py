2c322796b07f0076e39be1bd5c95ce5e728cd552
i-chernousov <iche7n@gmail.com>
2025-09-07 15:26:07 +0300

    Добавлено решение для Codewars

---
+++ codewars/8 kyu/convert_boolean_to_string.py
@@ -0,0 +1,15 @@
+"""
+Implement a function which convert the given
+boolean value into its string representation.
+
+Note: Only valid inputs will be given.
+"""
+
+
+def boolean_to_string(text: bool) -> str:
+    """Функция преобразует значение bool в строку"""
+    return str(text)
+
+
+if __name__ == "__main__":
+    print(boolean_to_string(False))
